b501d31e3d46e7f8d9e8a18f39c5ae2b977c0359
Sneha Damodaran <48755269+snehadamodaran1@users.noreply.github.com>
2022-07-26 10:42:55 +0530

    speakit commit

---
+++ eventPage.js
@@ -0,0 +1,17 @@
+var menuItem = {
+    "id" : "speak",
+    "title" : "Speak",
+    "contexts" : ["selection"]
+};
+chrome.contextMenus.create(menuItem);
+
+// function fixedEncodeURI(str){
+//     return encodeURI(str).replace(/%5B/g, '[').replace(/%5B/g, ']');
+// }
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if(clickData.menuItemId == "speak" && clickData.selectionText){
+        chrome.tts.speak(clickData.selectionText),{'rate' : 0.7}
+        
+    }
+})
